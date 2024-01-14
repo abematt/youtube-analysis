456c37db049f14f2a77932519e7bcae6c1c03bb6
abematt <abrahammathew.dev@gmail.com>
2024-01-13 21:36:28 -0600

    Added changes for matching youtube mobile share url

--- youtube-analyze/src/components/mainForm.jsx
+++ youtube-analyze/src/components/mainForm.jsx
@@ -25,6 +25,7 @@ function MainForm({
   function getYouTubeVideoID(url) {
     const regex =
       /(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+
     const match = url.match(regex);
     if (match && match[2].length == 11) {
       console.log("matching");
